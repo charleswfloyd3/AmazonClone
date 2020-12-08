e64c9eafe1f658d2ad08a9421a3885ab6cd4f873
Charles Floyd <charleswfloyd3@gmail.com>
2020-12-08 00:12:18 -0800

    added a slideshow and started creating deals with css grid

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function categorySelect(){
 function highlightsearchbar(){
     document.getElementById("navbarcenter").style.outline="3px solid orange"
 }
+
+let backgroundimages = ["backgroundimages/amazon_music.jpg", "backgroundimages/audibleplus.jpg", "backgroundimages/echoframes.jpg", "backgroundimages/halo.jpg", "backgroundimages/home.jpg", "backgroundimages/prepforholidays.jpg", "backgroundimages/primevideo.jpg", "backgroundimages/shopholidaydeals.jpg" ];
+let slideshowIndex = 0;
+function backgroundimageslideshow(){
+    if(slideshowIndex == backgroundimages.length ){
+            slideshowIndex = 0
+    };
+    document.add.src = backgroundimages[slideshowIndex];
+    slideshowIndex++;
+    setTimeout("backgroundimageslideshow()", 9000);
+};
+window.onload = backgroundimageslideshow;
